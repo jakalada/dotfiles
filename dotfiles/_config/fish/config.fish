c8e812b82b31c365fe8a33cd8311811ce38a8a31
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2019-01-01 04:42:05 +0900

    fisherコマンドによるプラグインのインストール先を変更

--- dotfiles/_config/fish/config.fish
+++ dotfiles/_config/fish/config.fish
@@ -45,3 +45,12 @@ function pfb --description 'Build by platformio'
 end
 
 fish_vi_key_bindings
+
+# Fisherでインストールするプラグインのパスを変更
+# REF: https://github.com/jorgebucaran/fisher#changing-the-installation-prefix
+set -g fisher_path ~/.config/fisher
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
