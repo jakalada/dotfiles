ed6afe9f5b4368d14bc84acf1df0376735795511
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2019-01-01 05:54:42 +0900

    change

--- dotfiles/_config/fish/config.fish
+++ dotfiles/_config/fish/config.fish
@@ -19,31 +19,12 @@ set ANDROID_HOME /usr/local/share/android-sdk
 #export IDF_PATH=~/esp/esp-idf
 #export PATH=~/esp/xtensa-esp32-elf/bin:$PATH
 
-# abbr
-alias g='git'
-alias r='rails'
-alias pf='platformio'
+alias ph='peco_select_history'
+alias pg='peco_select_ghq_repository'
 
 # replacement
 if which rmtrash >/dev/null
   alias rm='rmtrash'
-end
-
-# function like alias
-function pfr --description 'Build and upload by platformio'
-  set -l parent_dir $PWD/..
-  set -l src_dir $PWD/$argv[2]
-  set -lx PLATFORMIO_LIB_DIR $parent_dir
-  set -lx PLATFORMIO_SRC_DIR $src_dir
-  platformio run --target=upload -e $argv[1]; and platformio serialports monitor
-end
-
-function pfb --description 'Build by platformio'
-  set -l parent_dir $PWD/..
-  set -l src_dir $PWD/$argv[2]
-  set -lx PLATFORMIO_LIB_DIR $parent_dir
-  set -lx PLATFORMIO_SRC_DIR $src_dir
-  platformio run -e $argv[1]
 end
 
 # Fisherでインストールするプラグインのパスを変更
