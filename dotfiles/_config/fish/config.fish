0c7aad0ed5921fc8934a7f1b16d3cc137affc2ba
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2019-01-03 02:03:03 +0900

    change

--- dotfiles/_config/fish/config.fish
+++ dotfiles/_config/fish/config.fish
@@ -1,8 +1,8 @@
 fish_vi_key_bindings
 
 # fundle
-set fundle_plugins_dir $HOME/.config/fundle_plugins
-mkdir -p $fundle_plugins_dir
+set fundle_plugins_dir "$HOME/.config/fundle_plugins"
+mkdir -p "$fundle_plugins_dir"
 fundle plugin 'jethrokuan/z'
 fundle plugin '0rax/fish-bd'
 fundle plugin 'decors/fish-ghq'
@@ -10,20 +10,22 @@ fundle plugin 'oh-my-fish/plugin-peco'
 fundle plugin 'yoshiori/fish-peco_select_ghq_repository'
 fundle init
 
-# Homebrew
-set PATH /usr/local/bin $PATH
-
+# Java
+set JAVA_HOME (/usr/libexec/java_home -v 11)
+set PATH "$JAVA_HOME/bin" "$PATH"
 
 # Go
-set GOPATH ~/go
-set PATH $GOPATH/bin $PATH
+set GOPATH "$HOME/go"
+set PATH "$GOPATH/bin" $PATH
 
 # Ruby on HomeBrew
-set PATH  /usr/local/opt/ruby/bin $PATH
+set PATH /usr/local/opt/ruby/bin $PATH
 set PATH /usr/local/lib/ruby/gems/2.6.0/bin $PATH
 
 # Android
-set ANDROID_HOME /usr/local/share/android-sdk
+set ANDROID_HOME "$HOME/Library/Android/sdk"
+set PATH "$ANDROID_HOME/tools/bin" $PATH
+set PATH "$ANDROID_HOME/platform-tools" $PATH
 
 # esp-idf
 # (not set when using FreeRTOS)
