5152268844136f8c29fd3c477f541a6a9fa95623
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2019-06-16 23:57:14 +0900

    Java: change default version

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ jenv add "$(/usr/libexec/java_home -v 1.8)"
 jenv add "$(/usr/libexec/java_home -v 9)"
 jenv add "$(/usr/libexec/java_home -v 10)"
 jenv add "$(/usr/libexec/java_home -v 11)"
-jenv global 11
+jenv global 1.8
