bbbce468d70095cae2076b1b6de97e2fa65d85ab
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2020-05-07 00:16:42 +0900

    change

---
+++ README.md
@@ -0,0 +1,17 @@
+# dotfiles
+
+## Homebrew
+
+- [Homebrew/homebrew\-bundle](https://github.com/Homebrew/homebrew-bundle)
+
+### Install
+
+```
+$ brew bundle
+```
+
+### Dump
+
+```
+$ brew bundle dump # will create Brewfile
+```
--- install.sh
+++ install.sh
@@ -2,32 +2,6 @@
 
 # REF: https://qiita.com/youcune/items/fcfb4ad3d7c1edf9dc96
 set -eu
-
-# Homebrew
-# ========
-
-# Java
-brew tap AdoptOpenJDK/openjdk
-brew cask install adoptopenjdk8
-brew cask install adoptopenjdk9
-brew cask install adoptopenjdk10
-brew cask install adoptopenjdk11
-
-# jenv
-brew install jenv
-
-# vim
-brew install vim --HEAD
-
-# yarn
-brew install yarn
-
-# prettier
-brew install prettier
-
-# starship
-brew install starship
-
 # yarn
 # ===
 
