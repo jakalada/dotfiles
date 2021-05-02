95639484640da4bb0da56329054f1b128fec1d5f
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2021-05-02 22:21:54 +0900

    change

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ set -eu
 
 # jenv
 jenv add "$(/usr/libexec/java_home -v 1.8)"
-jenv add "$(/usr/libexec/java_home -v 9)"
-jenv add "$(/usr/libexec/java_home -v 10)"
-jenv add "$(/usr/libexec/java_home -v 11)"
-jenv global 1.8
+jenv add "$(/usr/libexec/java_home -v 14)"
+jenv global 14
